d98bc9a74a5d9cf016712c0fd6aa47502257b91b
Noor us Sabah <noorrazzaq038@gmail.com>
2022-06-18 01:27:39 +0500

    Added form features

--- src/Components/Pages/Purchase.jsx
+++ src/Components/Pages/Purchase.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react';
+import Stack from '@mui/material/Stack';
+import ButtonR from '@mui/material/Button';
+import {IoMdAdd} from 'react-icons/io';
+
 import PurchaseForm from '../Forms/PurchaseForm'
+import UpperNavbar from '../Navbars/UpperNavbar'
 
 export const AddPurchase = () => {
   return (<>
+    <UpperNavbar/>
     <div className="Forms"  style={{padding:'20px' , paddingTop:'60px'}}>
         {/* Form */}
     <PurchaseForm/>
@@ -11,7 +17,27 @@ export const AddPurchase = () => {
 }
 
 export const PurchaseList = () => {
-    return(
-        <div className="Purchase">Purchase List</div>
+    const [showImport, setShowImport] = useState(false);
+    const [showAddModal, setShowAddModal] = useState(false);
+
+    function hideModalImport(){
+        setShowImport(false);
+    }
+    function hideModalAdd(){
+      setShowAddModal(false);
+    }
+
+
+    return(<>        <div className='main_container' style={{padding:10}}>
+        <div  style={{paddingBottom:20,display:"flex",justifyContent:"space-between"}} >
+            <Stack spacing={2} direction="row">
+                <ButtonR variant="contained" startIcon={<IoMdAdd />} onClick = {() => setShowAddModal(!showAddModal)}>
+                    Add Purchase
+                </ButtonR>
+       
+       </Stack>
+       </div>
+       </div>
+       </>
     )
 }
